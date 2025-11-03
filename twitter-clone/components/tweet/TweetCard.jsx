e0505a2d4643645532ae309437aa4a2da04b78d6
gokcesen <sengkce@gmail.com>
2025-11-03 05:44:20 +0100

    feat: display username and handle in TweetCard

--- twitter-clone/components/tweet/TweetCard.jsx
+++ twitter-clone/components/tweet/TweetCard.jsx
@@ -1,24 +1,29 @@
 import TweetActions from "./TweetActions";
-
+import { FiUser } from "react-icons/fi";
 
 const TweetCard = ({ tweet }) => {
-   
-    return(
-        <>
-        <div
-               key={tweet.id}
-               className="p-6 w-[700px] mb-4"
-              >
-                <div>
-                    <h2 className="font-bold text-xl mb-3 text-gray-200">{tweet.title}</h2>
-                    <p className="text-gray-700 mb-4 leading-relaxed">{tweet.body}</p>
-                </div>
-                <div>
-                <TweetActions tweet={tweet} />
-                </div>
-               </div>
-        </>
-    );
-}
+	return (
+		<>
+			<div key={tweet.id} className="p-6 w-[700px] mb-2">
+				{/* Avatar + Username */}
+				<div className="flex items-center space-x-3 mb-3">
+					<div className="w-12 h-12 rounded-full bg-gray-600 flex items-center justify-center">
+						<FiUser size={32} className="text-white" />
+					</div>
+					<div className="flex flex-col">
+						<span className="font-semibold text-white">John Doe</span>
+						<span className="text-sm text-gray-400">@johndoe</span>
+					</div>
+				</div>
+
+				{/* Tweet body */}
+				<p className="text-white mb-3 leading-relaxed">{tweet.body}</p>
+
+				{/* Tweet actions */}
+				<TweetActions tweet={tweet} />
+			</div>
+		</>
+	);
+};
 
 export default TweetCard;
